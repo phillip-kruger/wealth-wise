009667d402fbb9e2aff67435ae1b175e22119608
Phillip Kruger <phillip.kruger@gmail.com>
2025-04-04 09:17:46 +1100

    Upgraded langchain4J version
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- src/main/java/com/wealthwise/investment/InvestmentService.java
+++ src/main/java/com/wealthwise/investment/InvestmentService.java
@@ -3,8 +3,10 @@ package com.wealthwise.investment;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
+import jakarta.enterprise.context.SessionScoped;
 
 @RegisterAiService
+@SessionScoped
 public interface InvestmentService {
 
 
